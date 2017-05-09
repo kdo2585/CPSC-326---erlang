ce78872e9212bae5e7f6a96955937d364cf85114
kdo2585 <kevindo0511@gmail.com>
2017-05-08 18:33:07 -0700

    almost done

--- searchengine.erl
+++ searchengine.erl
@@ -1,30 +1,30 @@
 -module(searchengine).
 	-export([tokenize/1]).
-    -export([inputKV/0]).
+        -export([inputKV/0]).
 	-export([createKV/2]).
 	-export([createmap/0]).
 	-export([comment_relevance/2]).
 	-export([comment_length/1]).
-	-export([add_comment_data/4]).
-	-export([get_all_lines/1]).
-	-export([readlines/1]).
+	-export([add_comment_data/3]).
 	-export([start/0]).
-	-export([sort/3]).
-
+	-export([sort/1]).
+	-export([store_comments/2]).
+	-export([read/1]).
 		% sort example:
 		% val -> score = 1, comments = 2, user_rate = 3
 		% (val, [{upvotes/score,  comment,  user rate}])
 		% lists:keysort(3,[{1500,what,5},{1000,climate,2},{2000,gas,3},{100,trump,1}]).
-		A = {1500,what,5}, B = {1000,climate,2}, C ={2000,gas,3}, D = {100,trump,1}.
-		
-		sort({Upvotes, Comments, Rank})-> 
-			lists:keysort(3,[A,B,C,D]).
+		%{string, relevance, score, length}
+
+
+		sort(Rank)-> 
+			lists:keysort(3,[Rank]).
 			
 		tokenize(String) ->
 			string:tokens(String, " ").
 	
 		inputKV()-> 
-			 tokenize( io:get_line("Enter keywords and values(1-6), separated by spaces ")).
+			 tokenize( io:get_line("Enter keywords and values(1-6), separated by spaces: ")).
 
 		createKV([],Map) -> 
 			Map;
@@ -48,28 +48,40 @@
 		
 		comment_length(String)-> length(tokenize(String)).
 		
-		add_comment_data(Id,R,L,S)-> {Id,R,L,S}.
-
-		%readlines(test).
-		readlines(parse_test_file.txt)->
-				{ok,Device} = file:open(parse_test_file.txt,[read]),
-				try get_all_lines(Device)
-					after file:close(Device)
-				end.
+		add_comment_data(Id,R,L)-> [{Id,R,L}].
 		
-		get_all_lines(Device)->
-				case io:get_line(Device,"") of
-					eof->[];
-					Line->Line++get_all_lines(Device)
-				end.
+		read(File) ->
+				{ok,Binary} = file:read_file(File),
+				Lines = string:tokens(erlang:binary_to_list(Binary),"\n").
+
+		store_comments([],Map)->
+				[];
+		
+		store_comments(ComLists, Map)->
+			Id = lists:nth(1,ComLists), 
+			R=comment_relevance(Id, Map), 
+			L=comment_length(Id),
+			lists:append(add_comment_data(Id,R,L), store_comments(lists:nthtail(1,ComLists),Map)).
 
 		start()->
-			
-			S="oranges apples sock tickle the flying oranges",
-			Q=string:tokens(string:to_lower(S)," "),
+			R = read(test),
 			M=createmap(),
+			S = store_comments(R, M),
+			G = io:get_line("How do you want to sort? (L = length, R= relevant)"),
+			if
+				G == "L\n" ->
+					X = 2;
+				true ->
+					X = 3
+			end,
+
+			lists:keysort(X,S),
+
+			%S="oranges apples sock tickle the flying oranges",
+			%Q=string:tokens(string:to_lower(S)," "),
+
 			
 			%spawn(parse, comment_relevance, [U,M]),
 			%spawn(parse, comment_relevance,[T,M]),
-			comment_relevance(Q,M).
+			%comment_relevance(Q,M).
 		
